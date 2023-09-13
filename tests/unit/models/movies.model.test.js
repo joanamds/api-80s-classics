4b6f5d5bcc53a4cb2500b9dc7a331f898cc18ed1
joanamds <joanamdsantos1@gmail.com>
2023-09-13 12:09:35 -0300

    :hammer: chore: configurando testes

---
+++ tests/unit/models/movies.model.test.js
@@ -0,0 +1,4 @@
+// const chai = require('chai');
+// const { expect } = chai;
+// const connection = require('../../../src/models/connection');
+// const sinon = require('sinon');
---
+++ tests/unit/services/genres.service.test.js
@@ -0,0 +1,3 @@
+// const chai = require('chai');
+// const { expect } = chai;
+// const sinon = require('sinon');
